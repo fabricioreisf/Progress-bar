af58be68e4b3f711374881e57190aec2dadf1caf
Fabrício R. Ferreira <fabricio.ferreira@novaenergia.com.br>
2025-05-09 19:34:26 -0300

    feature: increase button increases size 3:)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import ProgressBar from "./ProgressBar.jsx" 
 import React, { useState } from "react" 
+import "./App.css"
 
 export default function App() { 
   const [progress, setProgress] = useState(0); 
   
   function handleClick() { 
     setProgress(Math.min(100, progress + 10)); 
+    
   } 
   
   return ( 
     <h1> <ProgressBar progress={progress} /> 
       <button 
         className="progress-button"
-        onClick={handleClick} 
-        ></button> 
+        onClick={handleClick}
+        style={{
+          padding: `0.3em ${100 + 3*progress}px`
+        }}
+        >Armenta</button> 
     </h1> 
   ) 
 }
